9b973cf5388c713d57a6c76291ff02879380c0ba
ScanAlign <atifjan2019@gmail.com>
2026-05-10 23:59:49 +0500

    Add redirect from 24-hour tyre fitting to tyre fitting near me

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: "/24-hour-tyre-fitting",
+        destination: "/tyre-fitting-near-me",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
